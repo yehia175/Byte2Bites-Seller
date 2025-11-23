9b9e67bc24ca33f523650691785c5fe655818857
Alaa14Ahmed <aalaaahmed720@gmail.com>
2025-11-23 18:48:04 +0200

    delivery price under node seller always equal 0 so it does not appear in the app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,5 +77,6 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     kapt("com.github.bumptech.glide:compiler:4.16.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.android.material:material:1.10.0")
 
 }
